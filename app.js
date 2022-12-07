992fde36e375eff0a0dbef0d29f90d176b2daba3
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2022-12-07 13:35:00 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -3,42 +3,11 @@
         document.getElementById('c').value=y;
     }
 
-    function clr() {
+    function cls() {
         document.getElementById('c').value='';
     }
-
-/*function dis(value) {
-    document.getElementById("screen").value += value;
+    
+    function dis(val) {
+        document.getElementById('c').value+=val;
+        
     }
-
- 
-        function dis(value) {
-            document.getElementById("screen").value += value;
-            }
-            
-
-<script>
-var x = myFunction(4, 3);
-document.getElementById("").innerHTML = x;
-
-function myFunction(a, b) {
-  return a * b;
-}
-</script>
-
-
-<button onclick="displayDate()">The time is?</button>
-
-<script>
-function displayDate() {
-  document.getElementById("c").innerHTML = Date();
-}
-</script>
-
-<button onclick="1()">1</button>
-<script>
-    function 1() {
-        document.getElementById("c").innerHTML = 1();
-    }
-</script>
-*/
