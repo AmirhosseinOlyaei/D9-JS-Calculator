4ef7bcb6163e8c7a75857f179242478c86ac8cf1
amirhosseinolyaei <amirholyaei@gmail.com>
2022-12-07 11:17:06 -0500

    added functions to buttoms

---
+++ app.js
@@ -0,0 +1,44 @@
+    function solve() {
+        let y = eval(document.getElementById('c').value);
+        document.getElementById('c').value=y;
+    }
+
+    function clr() {
+        document.getElementById('c').value='';
+    }
+
+/*function dis(value) {
+    document.getElementById("screen").value += value;
+    }
+
+ 
+        function dis(value) {
+            document.getElementById("screen").value += value;
+            }
+            
+
+<script>
+var x = myFunction(4, 3);
+document.getElementById("").innerHTML = x;
+
+function myFunction(a, b) {
+  return a * b;
+}
+</script>
+
+
+<button onclick="displayDate()">The time is?</button>
+
+<script>
+function displayDate() {
+  document.getElementById("c").innerHTML = Date();
+}
+</script>
+
+<button onclick="1()">1</button>
+<script>
+    function 1() {
+        document.getElementById("c").innerHTML = 1();
+    }
+</script>
+*/
